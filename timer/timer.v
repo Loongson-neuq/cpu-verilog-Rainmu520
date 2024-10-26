e214efe9c8c9ff2103a647e11bfdab9651b4efd6
Rainmu520 <1775861673@qq.com>
2024-10-26 15:28:29 +0800

    完成作业

--- timer/timer.v
+++ timer/timer.v
@@ -1,16 +1,9 @@
-# ⚠　WARNING ⚠
-## Web action detected!
-## 检测到网页端提交！
+module timer(
+	input clk,
+    output reg [7:0] timer
+);
+always @(posedge clk) begin
+    timer <= timer + 1;
+end
 
-The commit was made by a web action, which is not allowed to use in this repository. It has been forced to rollback.
-网页端被禁止使用，该提交已经被强制撤回。
-    
-## Previous file page url:
-## 被撤回前文件的页面链接:
-
-[timer/timer.v@da1c1a7](https://github.com/Loongson-neuq/cpu-verilog-Rainmu520/blob/76c6955b85b3eca476268cf0cef41bdf2b970eff/timer%2Ftimer.v)
-
-## Previous file raw url:
-## 被撤回前文件的下载链接:
-
-[timer/timer.v@da1c1a7](https://github.com/Loongson-neuq/cpu-verilog-Rainmu520/raw/76c6955b85b3eca476268cf0cef41bdf2b970eff/timer%2Ftimer.v)
+endmodule
--- timer/timer_tb.v
+++ timer/timer_tb.v
@@ -1,16 +1,19 @@
-# ⚠　WARNING ⚠
-## Web action detected!
-## 检测到网页端提交！
-
-The commit was made by a web action, which is not allowed to use in this repository. It has been forced to rollback.
-网页端被禁止使用，该提交已经被强制撤回。
-    
-## Previous file page url:
-## 被撤回前文件的页面链接:
-
-[timer/timer_tb.v@3be2589](https://github.com/Loongson-neuq/cpu-verilog-Rainmu520/blob/98f6392a26c1f7f0390bf7183db62593043c9776/timer%2Ftimer_tb.v)
-
-## Previous file raw url:
-## 被撤回前文件的下载链接:
-
-[timer/timer_tb.v@3be2589](https://github.com/Loongson-neuq/cpu-verilog-Rainmu520/raw/98f6392a26c1f7f0390bf7183db62593043c9776/timer%2Ftimer_tb.v)
+module counter_tb;
+    reg clk;
+    wire [7:0] count;
+    counter uut (
+        .clk(clk),
+        .count(count)
+    );
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;  
+    end
+    initial begin
+        #0;
+        #100;
+        $stop;
+    end
+    initial begin
+        $monitor("Time: %0t, Count: %0d", $time, count);
+    end
